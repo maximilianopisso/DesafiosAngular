3dda41111bc5f6770c2a46f76033f499e1a9deb4
Maximiliano Pisso <maximiliano.pisso@gmail.com>
2021-12-28 12:01:30 -0300

    Creacion de servicio User , moficacion login service ok

--- primerProyecto/src/app/components/login/login.component.ts
+++ primerProyecto/src/app/components/login/login.component.ts
@@ -1,7 +1,10 @@
 import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/models/user.model';
+//import { User } from 'src/app/models/user.model';
 import { LoginService } from 'src/app/services/login.service';
+import { UserService } from 'src/app/services/user.service';
+
 
 @Component({
   selector: 'app-login',
@@ -9,16 +12,23 @@ import { LoginService } from 'src/app/services/login.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy, AfterViewInit {
-
+  max_user : number = 10;
+  muestra : User[] =[];
   users: User[] = [];
-  constructor(
-    private loginService: LoginService,
-  ) {
+
+  constructor(private loginService: LoginService) {
     console.log("LOGIN_COMPONENT - CONSTRUCTOR - CHECKED ");
   }
 
   ngOnInit(): void {
     console.log("LOGIN_COMPONENT - INIT - CHECKED ");
+    console.log("ESTOS SON LOS USARIOS VALIDOS");
+    this.users = this.loginService.getUsers();
+
+    for (let i=0;i<this.max_user;i++){
+      this.muestra.push(this.users[i])
+    }
+    console.table(this.muestra);
   }
 
   ngAfterViewInit(): void {
@@ -38,13 +48,14 @@ export class LoginComponent implements OnInit, OnDestroy, AfterViewInit {
   passwordControl = this.loginForm.controls['password'];
 
   loginValidate() {
+
     const valido = this.loginService.validateLogin(this.emailControl.value, this.passwordControl.value)
 
     if (valido) {
       console.log("Usuario y Contraseña son validos -> Ingresa")
     }
     else {
-      console.log("No se le permite el ingreso -> No valido")
+      console.log("No se le permite el ingreso -> No Valido")
     };
   }
 }
--- primerProyecto/src/app/services/user.service.ts
+++ primerProyecto/src/app/services/user.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { User } from '../models/user.model';
 
 @Injectable({
@@ -6,41 +8,21 @@ import { User } from '../models/user.model';
 })
 export class UserService {
 
-  private usuarios: User[] = [
-    // {
-    //   nombre: "Tamara",
-    //   apellido: "Sultano",
-    //   direccion: "Paraguay 2054",
-    //   movil: 34154568534,
-    //   email: "tsultano@gmail.com",
-    //   password: "tama1234",
-    // },
-    // {
-    //   nombre: "Maximiliano",
-    //   apellido: "Hernandez",
-    //   direccion: "Tucuman 154",
-    //   movil: 3415684579,
-    //   email: "mpisso@gmail.com",
-    //   password: "maxi1234",
-    // },
-    // {
-    //   nombre: "Felicia",
-    //   apellido: "Ramos",
-    //   direccion: "Centeno 54",
-    //   movil: 3415689535,
-    //   email: "framos@gmail.com",
-    //   password: "feli1234",
-    // },
-  ]
+  private url:string = "https://61bcb895d8542f00178249b1.mockapi.io/api/";
 
-  constructor() { }
+  private users: User[] = [];
 
+  constructor(private httpClient : HttpClient) {}
+
+
+  getUsers(): Observable<User[]> {
+      return this.httpClient.get<User[]>(`${this.url}persons`);
+  }
 
   // addUser(){}
 
-  // getUser(){}
+  // getUser(id:number){}
 
-  // getList():
 }
 
 

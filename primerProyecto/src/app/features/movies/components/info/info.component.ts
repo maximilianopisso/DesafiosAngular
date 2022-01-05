f270a5d70a62e9d9b1e112494d43985637c76ddc
Maximiliano Pisso <maximiliano.pisso@gmail.com>
2022-01-05 20:39:38 -0300

    popularidad con estrellas (revisar ngfor)

--- primerProyecto/src/app/features/movies/components/info/info.component.ts
+++ primerProyecto/src/app/features/movies/components/info/info.component.ts
@@ -15,7 +15,8 @@ export class InfoComponent implements OnInit, OnDestroy, AfterViewInit {
 
   movie: MovieAPI | any;
   urlPath: string = 'https://image.tmdb.org/t/p/w500';
-  popularidad : number[] =[];
+  popularidad_full_star : number[] =[];
+  popularidad_half_star : number[] =[];
   indexpopular:number =0;
 
   constructor(
@@ -36,15 +37,20 @@ export class InfoComponent implements OnInit, OnDestroy, AfterViewInit {
     this.moviesService.getDetailAPI(this.activateRoute.snapshot.params['id'])  //obtiene el id desde la ruta url a la llamada al componente
     .subscribe(respose => {this.movie = respose
       console.log(this.movie);
+      console.log("valoracion original",this.movie.vote_average);
 
       this.indexpopular = Math.round((this.movie.vote_average)/2);
-      console.log(this.indexpopular);
+      console.log("valoracion redondeada",this.indexpopular);
 
       for(let i=1; i<this.indexpopular; i++){
-        this.popularidad.push(1);
+        this.popularidad_full_star.push(1);
       }
 
-
+      this.indexpopular = (Math.round((this.movie.vote_average)/2))%2;
+      console.log("media estrella?",this.indexpopular);
+      if (this.indexpopular === 1){
+        this.popularidad_half_star.push(1);
+      }
 
    }); // obtiene el detalle de la pelicula y la carga en el campo movie del componente local.
   }
